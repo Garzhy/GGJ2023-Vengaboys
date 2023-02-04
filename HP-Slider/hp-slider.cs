add73e1822caeb250e861969c18ea3197c4a8006
Christian Masse <Christian.Masse@teambespin.us>
2023-02-04 10:41:27 -0600

    Updated add/sub hp function, getters and setters

--- HP-Slider/hp-slider.cs
+++ HP-Slider/hp-slider.cs
@@ -1,15 +1,36 @@
-class hp-slider
+public class Hp
 {
-    const int max = 10;
-    const int min = 0;
-    int current;
-    const int difference = 1;
+    private const int max;
+    private const int min;
+    private int current;
+    private const int dif = 1; //Adds and subtracts
 
-    public int SubHP(int current, int difference){
-        current = current - difference;
+    /// Initializes max, min, and current
+    public Hp(int giveMax, int giveMin){
+        max = giveMax;
+        min = giveMin;
+        current = giveMax;
+    }
+    /// Initializes max and min hp for main hp bar
+    public Hp(){
+        max = 10;
+        min = 0;
+    }
+    /// Gets current health 
+    public int getCurrent(){
         return current;
     }
-
+    /// Adds/Subtracts HP 
+    public int changeHP(bool positiveChange){
+        if(positiveChange){
+            current += dif;
+        }
+        else {
+            current -= dif;
+        }
+        return current;
+    }
+    /// Checks current HP to see if game over 
     public bool CheckHP(int current){
         if (current <= 0){
             return true;
